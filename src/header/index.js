189579665d1cc5fd2cff22f453a358c270618fe1
Julie Nginn <nginn.julie@gmail.com>
2018-12-18 16:01:47 +0100

    create header repo

--- src/header/index.js
+++ src/header/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './style.css'
 
 const Header = () => 
     <div className="header">
-      <img className="logo" src='../images/eos_logo.png' alt="logo"/>
-      <h1 className="title">Super Elevator</h1>
+      <a href="/">
+        <img className="logo" src='../images/eos_logo.png' alt="logo"/>
+        <h1 className="title">Super Elevator</h1>
+      </a>
     </div>
 
 export default Header
