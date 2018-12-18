5903a0efb91b03ef304db6db3a9610e39773694d
Julie Nginn <35365804+Madylune@users.noreply.github.com>
2018-12-18 17:15:14 +0100

    fix logo loading (#1)

--- src/header/index.js
+++ src/header/index.js
@@ -3,10 +3,8 @@ import './style.css'
 
 const Header = () => 
     <div className="header">
-      <a href="/">
-        <img className="logo" src='../images/eos_logo.png' alt="logo"/>
-        <h1 className="title">Super Elevator</h1>
-      </a>
+      <img className="logo" src={require('../images/eos_logo.png')} alt="logo"/>
+      <h1 className="title">Super Elevator</h1>
     </div>
 
 export default Header
