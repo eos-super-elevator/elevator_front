f4b890371161a62fc8c44e8c34189880db8b168b
Alexandre Rousseau <contact@rousseau-alexandre.Fr>
2019-01-11 11:49:07 +0100

    Remove socket on global componenet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react'
 import Header from './components/header'
 import Body from './components/body'
-import socketIOClient from 'socket.io-client'
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      response: false,
-      endpoint: 'http://192.168.1.140:3000'
     }
-  }
-
-  componentDidMount() {
-    const { endpoint } = this.state
-    const socket = socketIOClient(endpoint)
-    socket.on('new_elevator_state', data => this.setState({ response: data }))
   }
 
   render() {
